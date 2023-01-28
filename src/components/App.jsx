0d2bc48dab1e591da431467da585c15b2d7e34fa
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-01-28 20:34:22 +0200

    axios get

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,36 @@
-import  {Component} from 'react';
+import { Component } from 'react';
+import axios from 'axios';
 // import { ToastContainer, toast } from 'react-toastify';
 
 export default class App extends Component {
-    state={
-        pictures: [],
-    }
+  state = {
+    pictures: [],
+    loading: false,
+  };
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get(
+        'https://pixabay.com/api/?key=31909701-b05a4a73718479a7bf524b9e0&q=cat&page=1&&image_type=photo&orientation=horizontal&per_page=12'
+      )
+      .then(({ data }) => {
+        this.setState({ pictures: data.hits, loading: false });
+      });
+    //   .catch(error =>  )
+  }
   render() {
+    const { pictures, loading } = this.state;
+    const elements = pictures.map(({ id, webformatURL, largeImageURL }) => (
+      <li key={id}>
+        <img src={webformatURL} alt="" />
+      </li>
+    ));
     return (
-        <ul>
-            <li>
-                <img src="" alt="" />
-            </li>
-            <li>
-                <img src="" alt="" />
-            </li>
-            <li>
-                <img src="" alt="" />
-            </li>
-        </ul>
+        <>
+        {loading && <p>...Loading</p>}
+        <ul>{elements}</ul>;
+        </>
     )
+    
   }
 }
