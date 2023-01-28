e909f832e338c08b5d7238b0e6d83a405128de84
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-01-28 18:48:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
-import React, {Component} from "react";
-import { ToastContainer, toast } from 'react-toastify';
+import  {Component} from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
 
 export default class App extends Component {
-  
+    state={
+        pictures: [],
+    }
+  render() {
+    return (
+        <ul>
+            <li>
+                <img src="" alt="" />
+            </li>
+            <li>
+                <img src="" alt="" />
+            </li>
+            <li>
+                <img src="" alt="" />
+            </li>
+        </ul>
+    )
+  }
 }
